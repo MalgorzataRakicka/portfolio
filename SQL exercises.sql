4acacd2aa90dff001b5aa6e6a454b61e19142e5e
Małgorzata Rakicka <32996795+MalgorzataRakicka@users.noreply.github.com>
2024-07-02 18:25:42 +0200

    Update SQL exercises.sql - ex.2.

--- SQL exercises.sql
+++ SQL exercises.sql
@@ -23,3 +23,15 @@ select mems.firstname || ' ' || mems.surname as member,
 			(mems.memid != 0 and bks.slots*facs.membercost > 30)
 		)
 order by cost desc; 
+
+/*Ex. 2. How can you produce a list of all members who have used a tennis court? Include in your output the name of the court, 
+and the name of the member formatted as a single column. Ensure no duplicate data, and order by the member name followed by 
+the facility name. */
+
+SELECT DISTINCT cd.members.firstname || ' ' || cd.members.surname as member, cd.facilities.name AS facility
+FROM ((cd.bookings
+INNER JOIN cd.facilities ON cd.bookings.facid=cd.facilities.facid)
+INNER JOIN cd.members ON cd.bookings.memid = cd.members.memid)
+WHERE cd.facilities.name LIKE '%Tennis Court%'
+ORDER BY member,facility
+
