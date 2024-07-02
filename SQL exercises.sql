61ac4fc7902ab72d52c03c1bf87fe7ee4a9a65dd
Małgorzata Rakicka <32996795+MalgorzataRakicka@users.noreply.github.com>
2024-07-02 18:20:46 +0200

    Update SQL exercises.sql

--- SQL exercises.sql
+++ SQL exercises.sql
@@ -1,4 +1,6 @@
--- Ex. 1. How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost.
+/* Ex. 1. How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? Remember that guests have different costs 
+to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0. Include in your output the name of the facility, the name of the member formatted 
+as a single column, and the cost. Order by descending cost.*/ 
 
 select mems.firstname || ' ' || mems.surname as member, 
 	facs.name as facility, 
